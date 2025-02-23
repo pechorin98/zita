503f44c0f48d795fa0c8fe3a584a23f74ab4236d
pechorin98 <buluthanyumuk26@gmail.com>
2025-02-23 16:43:16 +0300

    main

--- src/analytics.js
+++ src/analytics.js
@@ -3,7 +3,7 @@ import ReactGA from 'react-ga4';
 
 // Replace 'G-XXXXXXXXXX' with your actual GA4 Measurement ID
 export const initGA = () => {
-  ReactGA.initialize('G-295738192');
+  ReactGA.initialize('G-CY1N86K8X4');
   // Optionally, send the first pageview when the app loads
   ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search });
 };
