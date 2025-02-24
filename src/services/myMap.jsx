33c0000c873147f2641f475439189a32d508205c
pechorin98 <buluthanyumuk26@gmail.com>
2025-02-24 21:37:55 +0300

    main

--- src/services/myMap.jsx
+++ src/services/myMap.jsx
@@ -2,12 +2,24 @@ import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css"; // Import Leaflet CSS
 import styles from './myMapFooter.module.css';
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
+// Default ikonları manuel olarak ayarla
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 const MyMap = () => {
   const position = [41.0678, 28.7674]; // Default map center
 
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false}  className={styles.MapContainerComp}style={{ height: "40vh", width: "40%", borderRadius: "12px" }}>
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false} className={styles.MapContainerComp} style={{ height: "40vh", width: "40%", borderRadius: "12px" }}>
       <TileLayer
         attribution='&copy;'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
