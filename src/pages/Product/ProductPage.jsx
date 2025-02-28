3d472ebf515fd99c49a6fb3f5bd589139694b809
pechorin98 <buluthanyumuk26@gmail.com>
2025-02-28 12:37:06 +0300

    update2

--- src/pages/Product/ProductPage.jsx
+++ src/pages/Product/ProductPage.jsx
@@ -8,6 +8,7 @@ import Type3Comp from '../../components/mid/type3Comp/Type3Comp';
 import Type4Comp from '../../components/mid/type4Comp/Type4Comp';
 import { useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
+import PrimaryButton from '../../components/micro/primaryDesktopButton/PrimaryButton';
 
 function Product() {
 
@@ -33,7 +34,7 @@ function Product() {
         <section style={{ "overflowX": "hidden" }}>
             <Helmet>
                 <title>Ürünlerimiz | Bahçe ve Dış Mekan Aydınlatma Çözümleri - Zita Aydınlatma</title>
-                <meta name="description" content="Zita Aydınlatma, dış mekan ve bahçe aydınlatma çözümlerinde güvenilir üreticinizdir. Avrupa standartlarına uygun, su geçirmez, orijinal ABS gövdeli ve sararmalara karşı UV korumalı aydınlatma ürünleri." />
+                <meta name="description" content="Zita Aydınlatma, dış mekan ve bahçe aydınlatma çözümlerinde güvenilir üreticinizdir. Avrupa standartlarına uygun, su geçirmez, orijinal ABS gövdeli ve sararmalara karşı UV korumalı aydınlatma ürünleri." />
                 <link rel="canonical" href="https://www.zitaaydinlatma.com.tr/products" />
                 <meta name="robots" content="index, follow" />
             </Helmet>
@@ -64,12 +65,24 @@ function Product() {
                     text3={t('products.sockets.features3')} version="containerLeftText" Component={Type4Comp} src="/pages/products/socketIcon.webp" />
             </section>
             {/*Sertifika Components*/}
-            <Type2CompImgAnimation title={t('products.certifications.title')}
-                text={t('products.certifications.description')} imgContainerVersion="imgContainerIcon" contextContainerVersion="contextContainer"
-                src1="/pages/products/cekucuk.webp" src2="/pages/products/rohskucuk.webp" src3="/pages/products/isokucuk.webp" version="wrapperSigle" isButton
-                buttonText={t('home.certifications.button')} buttonRoute={`/${lng}/Certifications`} />
+            <section className={styles.certContainer}>
+                <div className={styles.certWrapper}>
+                    <h1 className='titleText'>{t('products.certifications.title')}</h1>
+                    <p className='paraText'>{t('products.certifications.description')}</p>
+                    <div className={styles.imgContainer}>
+                        <img src="/pages/products/cekucuk.webp" alt="Zita Aydınlatma Güvenlik ve Kalite Sertifikaları"/>
+                        <img src="/pages/products/rohskucuk.webp" alt="Zita Aydınlatma Güvenlik ve Kalite Sertifikaları"/>
+                        <img src="/pages/products/isokucuk.webp" alt="Zita Aydınlatma Güvenlik ve Kalite Sertifikaları"/>
+                    </div>
+                    <PrimaryButton text={t('home.certifications.button')} navRoute={`/${lng}/Certifications`}/>
+                </div>
+            </section>
         </section>
     );
 }
 
 export default Product;
+/*            <Type2CompImgAnimation title={t('products.certifications.title')}
+                text={t('products.certifications.description')} imgContainerVersion="imgContainerIcon" contextContainerVersion="contextContainer"
+                src1="/pages/products/cekucuk.webp" src2="/pages/products/rohskucuk.webp" src3="/pages/products/isokucuk.webp" version="wrapperSigle" isButton
+                buttonText={t('home.certifications.button')} buttonRoute={`/${lng}/Certifications`} /> */
